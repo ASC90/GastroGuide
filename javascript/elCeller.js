c5db45c96dfbc1ea17201755dc3b4177c5f9f9d0
Arnau <arnausantoscabre@gmail.com>
2017-12-22 10:29:52 +0000

    Actualizar URL ElCeller

--- javascript/elCeller.js
+++ javascript/elCeller.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+var elCellerUrl = "http://www.mocky.io/v2/5a3ccf90310000b810b592de";
+
+function openAndLoad (pUrl)
+{
+	var jsonObj = {};
+	var request = new XMLHttpRequest();
+	request.open ("GET", pUrl);
+	request.onload = function ()
+	{
+		if (this.status === 200)
+		{
+			console.log("Éxito!");		
+		}
+		else
+		{
+			console.log("Fracaso!");
+		}
+		jsonObj = JSON.parse(request.response);
+		console.log (request.response);
+	};
+	request.send();	
+}
+
+openAndLoad(elCellerUrl);
