cc0e4f78884d9bbf3262dd35e86b8f60826b3cc4
Unknown <apl.bcn@gmail.com>
2018-01-08 16:40:49 +0100

    filtrar funcional

--- javascript/filatrar_results.js
+++ javascript/filatrar_results.js
@@ -5,10 +5,18 @@ function Ajax(method, url, onSuccess) {
             onSuccess(JSON.parse(this.responseText));
         }
     };
-
     getData.open(method, url, true);
     getData.send();
 };
+
+function composicion(como, donde, lista) {
+    let contenido = "";
+    lista.forEach(composicion => {
+        contenido += como.renderizar(composicion);
+    })
+    donde.innerHTML += contenido;
+};
+
 class Buscador {
     constructor() {
         this.resultados = [];
@@ -16,9 +24,11 @@ class Buscador {
     obtenerData() {
         let fichas = document.getElementById("section-fichas");
         Ajax("GET", "http://www.mocky.io/v2/5a5355223000002b1e1ebebe", (data) => {
+            let contenido = "";
             data.forEach(restaurante => {
-                fichas.innerHTML += Restaurante.renderizar(restaurante);
+                contenido += Restaurante.renderizar(restaurante);
             });
+            fichas.innerHTML += contenido;
         });
     }
 };
@@ -47,5 +57,58 @@ class Restaurante {
     `;
     }
 };
+class Filtrado {
+    constructor() {
+        this.listas = null; //esto sirve para indicar que no tiene nada.
+    }
+    obtenerDatos() {
+        let filtros = document.getElementById("chk-tipo-cocina");
+        let buscando = document.getElementById("selc-cocina");
+        Ajax("GET", "http://www.mocky.io/v2/5a3b8528300000aa0e82d1e4", (datos) => {
+            composicion(Cocina, filtros, datos.tipoDeCocina);
+            composicion(BuscadorCocina, buscando, datos.tipoDeCocina);
+        });
+    };
+};
+class Cocina {
+    static renderizar(datos) {
+        return `
+        <input type="checkbox" name="tipo-cocina" value="${datos}">${datos}
+        <br>
+    `;
+    }
+};
+class BuscadorCocina {
+    static renderizar(datos) {
+        return `
+       <option value="${datos}"> ${datos}</option>
+    `;
+    }
+};
+
+class FiltroAmbientes {
+    obtenerAmbientes() {
+        let filtros = document.getElementById("chk-ambientes");
+        Ajax("GET", "http://www.mocky.io/v2/5a3b8525300000e40e82d1e3", (datos) => {
+            let contenido = "";
+            datos.tipoDeAmbiente.forEach(ambiente => {
+                contenido += Ambientes.renderizar(ambiente);
+            });
+            filtros.innerHTML += contenido
+        });
+    };
+};
+class Ambientes {
+    static renderizar(datos) {
+        return `
+       <input type="checkbox" name="ambientes" value="${datos}">${datos}
+       <br>
+    `;
+    }
+};
 let buscando = new Buscador();
 buscando.obtenerData();
+let filtrando = new Filtrado();
+filtrando.obtenerDatos();
+let ambientando = new FiltroAmbientes();
+ambientando.obtenerAmbientes();
