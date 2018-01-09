bc11ebd754e9aa233322aa8e31be03629d652a40
Unknown <apl.bcn@gmail.com>
2018-01-09 15:39:09 +0100

    ajax post

--- javascript/general.js
+++ javascript/general.js
@@ -1,13 +1,82 @@
+///// Recoger todos los datos de un form
+function serialize(form) {
+    var data;
+    if (!form || form.nodeName !== "FORM") {
+        return;
+    }
+    var i, j, q = [];
+    for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+        if (form.elements[i].name === "") {
+            continue;
+        }
+        console.log(form.elements[i].nodeName, form.elements[i].value, form.elements[i].type)
+
+        switch (form.elements[i].nodeName.toUpperCase()) {
+            case 'INPUT':
+                switch (form.elements[i].type) {
+                    case 'text':
+                    case 'date':
+                    case 'search':
+                    case 'hidden':
+                    case 'password':
+                    case 'button':
+                    case 'reset':
+                    case 'submit':
+                        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                        break;
+                    case 'checkbox':
+                    case 'radio':
+                        if (form.elements[i].checked) {
+                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                        }
+                        break;
+                }
+                break;
+            case 'file':
+                break;
+            case 'TEXTAREA':
+                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                break;
+            case 'SELECT':
+                switch (form.elements[i].type) {
+                    case 'select-one':
+                        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                        break;
+                    case 'select-multiple':
+                        for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+                            if (form.elements[i].options[j].selected) {
+                                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+                            }
+                        }
+                        break;
+                }
+                break;
+            case 'BUTTON':
+                switch (form.elements[i].type) {
+                    case 'reset':
+                    case 'submit':
+                    case 'button':
+                        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                        break;
+                }
+                break;
+        }
+    }
+    data = q.join("&");
+    console.log(q);
+    return data;
+}
 ///// Request de datos al servidor
-function Ajax(method, url, onSuccess) {
+function Ajax(method, url, onSuccess, pinfo) {
     let getData = new XMLHttpRequest();
+    let info = pinfo || null;//email=e@e.es&pass=xxx
     getData.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             onSuccess(JSON.parse(this.responseText));
         }
     };
     getData.open(method, url, true);
-    getData.send();
+    getData.send(info);
 };
 
 ////// Se utiliza para concatenar el codigo HTML
@@ -45,27 +114,27 @@ buscandoCocina.obtenerDatos();
 function ValidationBusqueda(evento) {
     evento.preventDefault();
     var mql = window.matchMedia('screen and (min-width:320px) and (max-width:480px)');
-    var ok=true;
+    var ok = true;
     let adressval = /^[\s 0-9a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]+$/;
     if (adressval.test(buscador.buscar_adress.value) == false) {
         document.getElementById("errorbusqueda").innerText = "Por favor introduzca una dirección valida";
         document.getElementById("errorbusqueda").className = "FormResError";
-        ok=false;
+        ok = false;
     }
 
     if (!buscador.buscar_adress.value) {
         document.getElementById("errorbusqueda").innerText = "Por favor introduzca una localidad";
         document.getElementById("errorbusqueda").className = "FormResError";
-        ok=false;
+        ok = false;
     }
 
     if (!mql.matches && !buscador.fecha.value && buscador.buscar_adress.value) {
         document.getElementById("errorbusqueda").innerText = "";
         document.getElementById("errorfecha").innerText = "Por favor introduzca una fecha";
         document.getElementById("errorfecha").className = "FormResError";
-        ok=false;
+        ok = false;
     }
-    
+
     if (ok) {
         window.location.href = 'filtrar.html';
     }
