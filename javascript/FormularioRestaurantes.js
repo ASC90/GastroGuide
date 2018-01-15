f8daff3c54d4af95c1181b29129157f9458d635e
Daniil <dmihhedko@gmail.com>
2018-01-15 11:52:28 +0100

    formulario_restaurantes sin header footer

--- javascript/FormularioRestaurantes.js
+++ javascript/FormularioRestaurantes.js
@@ -6,6 +6,19 @@ function validarFormularioRestaurantes(event1) {
     var ok = true;
 
     ///////////////////////////////////////////LIMPIAR LOS FORMULARIOS//////////////////////////////////////////////////
+    document.getElementById("errorrestaurante").className = "";
+    document.getElementById("errornombre").className = "";
+    document.getElementById("errorapellidos").className = "";
+    document.getElementById("errorres-genero").className = "";
+    document.getElementById("erroremail").className = "";
+    document.getElementById("errorpassword").className = "";
+    document.getElementById("errorpassword2").className = "";
+    document.getElementById("errortel").className = "";
+    document.getElementById("errorcp").className = "";
+    document.getElementById("errorpoblacion").className = "";
+    document.getElementById("errorcalle").className = "";
+    document.getElementById("errornumero").className = "";
+    document.getElementById("errorrescheck").className = "";
     document.getElementById("errorrestaurante").innerText = "";
     document.getElementById("errornombre").innerText = "";
     document.getElementById("errorapellidos").innerText = "";
@@ -19,10 +32,14 @@ function validarFormularioRestaurantes(event1) {
     document.getElementById("errorcalle").innerText = "";
     document.getElementById("errornumero").innerText = "";
     document.getElementById("errorrescheck").innerText = "";
+
+    // $("#errorrestaurante").html("").removeClass("alert alert-danger")
+
+    
     // ///////////////////////////////////////////NOMBRE RESTAURANTE///////////////////////////////////////////////////////
     if (formularioRestaurantes.restaurante.value == "" || formularioRestaurantes.restaurante.value == null) {
         document.getElementById("errorrestaurante").innerText = "Por favor introduzca el nombre de su restaurante";
-        document.getElementById("errorrestaurante").className = "FormResError";
+        document.getElementById("errorrestaurante").className = "alert alert-danger";
         ;
     }
     ///////////////////////////////////////////NOMBRE CLIENTE///////////////////////////////////////////////////////
@@ -31,13 +48,13 @@ function validarFormularioRestaurantes(event1) {
     var nombrereg = /^[\s 0-9a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]+$/;
     if (nombrereg.test(formularioRestaurantes.nombre.value) == false) {
         document.getElementById("errornombre").innerText = "Su nombre no puede contener números o caracteres especiales";
-        document.getElementById("errornombre").className = "FormResError";
+        document.getElementById("errornombre").className = "alert alert-danger";
         ok = false;
     }
 
     if (formularioRestaurantes.nombre.value == "" || formularioRestaurantes.nombre.value == null) {
         document.getElementById("errornombre").innerText = "Por favor introduzca su nombre";
-        document.getElementById("errornombre").className = "FormResError";
+        document.getElementById("errornombre").className = "alert alert-danger";
         ok = false;
     }
     ///////////////////////////////////////////APELLIDOS///////////////////////////////////////////////////////
@@ -45,19 +62,19 @@ function validarFormularioRestaurantes(event1) {
     var apellidosreg = /^[\s a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]+$/;
     if (apellidosreg.test(formularioRestaurantes.apellidos.value) == false) {
         document.getElementById("errorapellidos").innerText = "Su apellido no puede contener números o caracteres especiales";
-        document.getElementById("errorapellidos").className = "FormResError";
+        document.getElementById("errorapellidos").className = "alert alert-danger";
         ok = false;
     }
 
     if (formularioRestaurantes.apellidos.value == "" || formularioRestaurantes.apellidos.value == null) {
         document.getElementById("errorapellidos").innerText = "Por favor introduzca sus apellidos";
-        document.getElementById("errorapellidos").className = "FormResError";
+        document.getElementById("errorapellidos").className = "alert alert-danger";
         ok = false;
     }
     ///////////////////////////////////////////GENERO///////////////////////////////////////////////////////
     if (formularioRestaurantes.genero.value == "" || formularioRestaurantes.genero.value == null) {
         document.getElementById("errorres-genero").innerText = "Por favor seleccione su género";
-        document.getElementById("errorres-genero").className = "FormResError";
+        document.getElementById("errorres-genero").className = "alert alert-danger";
         ok = false;
     }
     /////////////////////////////////////////CORREO///////////////////////////////////////////////////////
@@ -66,12 +83,12 @@ function validarFormularioRestaurantes(event1) {
 
     if (emailreg.test(formularioRestaurantes.email.value) == false) {
         document.getElementById("erroremail").innerText = "Por favor introduzca su correo electrónico en formato correcto";
-        document.getElementById("erroremail").className = "FormResError";
+        document.getElementById("erroremail").className = "alert alert-danger";
         ok = false;
     }
     if (formularioRestaurantes.email.value == "" || formularioRestaurantes.email.value == null) {
         document.getElementById("erroremail").innerText = "Por favor introduzca su correo electrónico";
-        document.getElementById("erroremail").className = "FormResError";
+        document.getElementById("erroremail").className = "alert alert-danger";
         ok = false;
     }
 
@@ -82,13 +99,13 @@ function validarFormularioRestaurantes(event1) {
 
     if (passwordreg.test(formularioRestaurantes.password.value) == false) {
         document.getElementById("errorpassword").innerText = "Su contraseña debe contener por lo menos 8 caracteres y contener una letra mayúscula, una minúscula y un número";
-        document.getElementById("errorpassword").className = "FormResError";
+        document.getElementById("errorpassword").className = "alert alert-danger";
         ok = false;
     }
 
     if (formularioRestaurantes.password.value == "" || formularioRestaurantes.password.value == null) {
         document.getElementById("errorpassword").innerText = "Por favor introduzca una contraseña";
-        document.getElementById("errorpassword").className = "FormResError";
+        document.getElementById("errorpassword").className = "alert alert-danger";
         ok = false;
     }
 
@@ -96,13 +113,13 @@ function validarFormularioRestaurantes(event1) {
     var pass2 = document.getElementById("password2");
     if (pass.value != pass2.value) {
         document.getElementById("errorpassword2").innerText = "Las contraseñas no coinciden";
-        document.getElementById("errorpassword2").className = "FormResError";
+        document.getElementById("errorpassword2").className = "alert alert-danger";
         ok = false;
     }
 
     if (formularioRestaurantes.password2.value == "" || formularioRestaurantes.password2.value == null) {
         document.getElementById("errorpassword2").innerText = "Por favor confirme su contraseña";
-        document.getElementById("errorpassword2").className = "FormResError";
+        document.getElementById("errorpassword2").className = "alert alert-danger";
         ok = false;
     }
 
@@ -115,13 +132,13 @@ function validarFormularioRestaurantes(event1) {
     var telefonoreg = /^(?=.*[0-9])(?=.{8})/;
     if (telefonoreg.test(formularioRestaurantes.telefono.value) == false) {
         document.getElementById("errortel").innerText = "Por favor introduzca su teléfono en formato correcto";
-        document.getElementById("errortel").className = "FormResError";
+        document.getElementById("errortel").className = "alert alert-danger";
         ok = false;
     }
 
     if (formularioRestaurantes.telefono.value == "" || formularioRestaurantes.telefono.value == null) {
         document.getElementById("errortel").innerText = "Por favor introduzca su teléfono";
-        document.getElementById("errortel").className = "FormResError";
+        document.getElementById("errortel").className = "alert alert-danger";
         ok = false;
     }
 
@@ -131,32 +148,32 @@ function validarFormularioRestaurantes(event1) {
     var cpreg = /^(?=.*[0-9])(?=.{5})/;
     if (cpreg.test(formularioRestaurantes.cp.value) == false) {
         document.getElementById("errorcp").innerText = "Por favor introduzca su código postal en formato correcto";
-        document.getElementById("errorcp").className = "FormResError";
+        document.getElementById("errorcp").className = "alert alert-danger";
         ok = false;
     }
 
     if (formularioRestaurantes.cp.value == "" || formularioRestaurantes.cp.value == null) {
         document.getElementById("errorcp").innerText = "Por favor introduzca su código postal";
-        document.getElementById("errorcp").className = "FormResError";
+        document.getElementById("errorcp").className = "alert alert-danger";
         ok = false;
     }
 
     ///////////////////////////////////////////POBLACION///////////////////////////////////////////////////////
     if (formularioRestaurantes.poblacion.value == "" || formularioRestaurantes.poblacion.value == null) {
         document.getElementById("errorpoblacion").innerText = "Por favor introduzca su población";
-        document.getElementById("errorpoblacion").className = "FormResError";
+        document.getElementById("errorpoblacion").className = "alert alert-danger";
         ok = false;
     }
     ///////////////////////////////////////////CALLE///////////////////////////////////////////////////////
     if (formularioRestaurantes.calle.value == "" || formularioRestaurantes.calle.value == null) {
         document.getElementById("errorcalle").innerText = "Por favor introduzca su calle, vía, etc.";
-        document.getElementById("errorcalle").className = "FormResError";
+        document.getElementById("errorcalle").className = "alert alert-danger";
         ok = false;
     }
     ///////////////////////////////////////////NUMERO///////////////////////////////////////////////////////
     if (formularioRestaurantes.numero.value == "" || formularioRestaurantes.numero.value == null) {
         document.getElementById("errornumero").innerText = "Por favor introduzca su número";
-        document.getElementById("errornumero").className = "FormResError";
+        document.getElementById("errornumero").className = "alert alert-danger";
         ok = false;
     }
 
@@ -165,7 +182,7 @@ function validarFormularioRestaurantes(event1) {
     if (document.getElementById("rescheckcheck").checked == false) {
 
         document.getElementById("errorrescheck").innerText = "Tiene que aceptar las condiciones legales";
-        document.getElementById("errorrescheck").className = "FormResError";
+        document.getElementById("errorrescheck").className = "alert alert-danger";
         ok = false;
     }
 
